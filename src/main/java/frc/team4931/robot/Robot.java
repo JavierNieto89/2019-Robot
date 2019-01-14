56008d108beee11230750a62fd12e64ddf791be0
James Crane <james@cranesnest.us>
2019-01-13 22:07:22 -0600

    Stubbed out main Robot class

---
+++ src/main/java/frc/team4931/robot/Robot.java
@@ -0,0 +1,56 @@
+package frc.team4931.robot;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+
+class Robot extends IterativeRobot {
+
+  @Override
+  public void robotInit() {
+    super.robotInit();
+  }
+
+  @Override
+  public void robotPeriodic() {
+    super.robotPeriodic();
+  }
+
+  @Override
+  public void disabledInit() {
+    super.disabledInit();
+  }
+
+  @Override
+  public void disabledPeriodic() {
+    super.disabledPeriodic();
+  }
+
+  @Override
+  public void autonomousInit() {
+    super.autonomousInit();
+  }
+
+  @Override
+  public void autonomousPeriodic() {
+    super.autonomousPeriodic();
+  }
+
+  @Override
+  public void teleopInit() {
+    super.teleopInit();
+  }
+
+  @Override
+  public void teleopPeriodic() {
+    super.teleopPeriodic();
+  }
+
+  @Override
+  public void testPeriodic() {
+    super.testPeriodic();
+  }
+
+  @Override
+  public void testInit() {
+    super.testInit();
+  }
+}
