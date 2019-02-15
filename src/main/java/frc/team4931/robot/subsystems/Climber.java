bdf7e44f0636967298a3e95c39e5d4d20ebe4f52
James Crane <james@cranesnest.us>
2019-02-14 19:28:15 -0600

    Removed imports for spark

--- src/main/java/frc/team4931/robot/subsystems/Climber.java
+++ src/main/java/frc/team4931/robot/subsystems/Climber.java
@@ -1,8 +1,6 @@
 package frc.team4931.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.command.Subsystem;
