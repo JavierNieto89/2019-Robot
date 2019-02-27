790eed09dbd9ca6412b713184b9aed2ee4d9fac0
James Crane <james@cranesnest.us>
2019-02-26 19:52:37 -0600

    Added NONE in Angles

--- src/main/java/frc/team4931/robot/enums/Angles.java
+++ src/main/java/frc/team4931/robot/enums/Angles.java
@@ -2,7 +2,7 @@ package frc.team4931.robot.enums;
 
 public enum Angles {
   FORWARD(0), FORWARD_RIGHT(30), RIGHT(90), BACKWARD_RIGHT(150), BACKWARD(180),
-  BACKWARD_LEFT(-150), LEFT(-90), FORWARD_LEFT(-30);
+  BACKWARD_LEFT(-150), LEFT(-90), FORWARD_LEFT(-30), NONE(-1);
 
   private int angle;
 
